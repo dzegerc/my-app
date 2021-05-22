e45e1ee66b237ce4162b89a7612d24ffdd49aab2
Dora Zegerc <dorazegerc13@gmail.com>
2021-05-22 11:15:32 +0200

    hands-on

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
-import React, { useEffect } from 'react';
-import { Route, useLocation } from 'react-router-dom';
-import './App.scss';
+import React, { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
+import Articles from './components/Articles/Articles';
 
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import Footer from './components/Footer/Footer';
+const App = () => {
+  const {isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
 
-import Home from './pages/Home/Home';
-import Event from './pages/Event/Event';
-import Events from './pages/Events/Events';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import Admin from './pages/Admin/Admin';
-
-const App = ()  => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0,0);
-  }, [pathname]);
-  useEffect(() => {
-    localStorage.getItem("isAdmin");
-    localStorage.getItem("authToken");
-  }, []);
-
-    return (
-      <>
-      <Header />
-      <Main>
-          <Route exact path="/" component={Home} />
-          <Route path="/events" component={Events} />
-          <Route path="/event/:id" component={Event}  />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/admin" component={Admin} />
-      </Main>
-      <Footer/>
+  const handleAuth = () => {
+    setIsLoggedIn(!isLoggedIn);
+  }
+  return(
+    <>
+      <button on onClick={handleAuth}>{isLoggedIn ? 'Logout' : 'Login'}</button>
+      <Articles />
     </>
   );
 }
--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 const Article = (props) => {
+    const {isLoggedIn} = useContext(AuthContext);
+    
     return (
         <>
         <h2>Some article title</h2>
-        <p>Description content...</p>
-        <button>Edit</button>
+        {isLoggedIn &&   <button>Edit</button> }
         </>
     );
 }
